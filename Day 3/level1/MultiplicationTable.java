79019d78dc0ef5411f471894ef8628e3ae366ea0
Tapesh Sharma <114461563+tapesh1134@users.noreply.github.com>
2025-06-13 09:00:21 +0530

    Delete Day 3 directory

--- Day 3/level1/MultiplicationTable.java
+++
@@ -1,8 +0,0 @@
-public class MultiplicationTable {
-    public static void main(String[] args) {
-        int number = 0; 
-        for (int i = 6; i <= 9; i++) {
-            System.out.println(number + " * " + i + " = " + (number * i));
-        }
-    }
-}
