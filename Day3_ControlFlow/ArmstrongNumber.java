20ac26934850350b1416bf59b5c2cb5e499d970b
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-16 09:54:58 +0530

    Refactor: file name of Armstrong number

--- Day3_ControlFlow/ArmstrongNumber.java
+++ Day3_ControlFlow/ArmstrongNumber.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class ArmstrongNumeber {
+public class ArmstrongNumber {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
