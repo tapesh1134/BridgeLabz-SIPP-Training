58edcbf942cea6c6dcc7d972afbe7efd889a8444
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-17 10:03:35 +0530

    Added: Implement program to convert a 2D array to a 1D array

---
+++ JavaArrays/twod_array_to1d.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class twod_array_to1d {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = sc.nextInt();
+        int[][] matrix = new int[rows][columns];
+        System.out.println("Enter the elements of the 2D array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        int[] array = new int[rows * columns];
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[index++] = matrix[i][j];
+            }
+        }
+        System.out.println("The elements of the 1D array are:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        sc.close();
+    }
+}
