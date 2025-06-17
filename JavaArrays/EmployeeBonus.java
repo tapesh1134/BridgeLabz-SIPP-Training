01842b6ffe5e805d43fbbd8c636900777aac8621
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-17 09:14:23 +0530

    Added: Implement Employee Bonus calculation program

---
+++ JavaArrays/EmployeeBonus.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] salary = new double[10];
+        double[] yearsOfService = new double[10];
+        double[] bonus = new double[10];
+        double[] newSalary = new double[10];
+        double totalBonus = 0;
+        double totalOldSalary = 0;
+        double totalNewSalary = 0;
+        for (int i = 0; i < 10; ) {
+            double sal = sc.nextDouble();
+            double yos = sc.nextDouble();
+            if (sal < 0 || yos < 0) {
+                System.out.println("Invalid input, please enter again.");
+                continue;
+            }
+            salary[i] = sal;
+            yearsOfService[i] = yos;
+            i++;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (yearsOfService[i] > 5) {
+                bonus[i] = salary[i] * 0.05;
+            } else {
+                bonus[i] = salary[i] * 0.02;
+            }
+            newSalary[i] = salary[i] + bonus[i];
+            totalBonus += bonus[i];
+            totalOldSalary += salary[i];
+            totalNewSalary += newSalary[i];
+        }
+        System.out.println("Total bonus payout: " + totalBonus);
+        System.out.println("Total old salary: " + totalOldSalary);
+        System.out.println("Total new salary: " + totalNewSalary);
+        sc.close();
+    }
+}
