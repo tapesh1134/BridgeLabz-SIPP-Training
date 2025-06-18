3f40ad7809a06e96520151186c625ed847e5e1c7
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 11:27:24 +0530

    Refactor: improve code formatting in QuotientRemainder class

--- Methods/QuotientRemainder.java
+++ Methods/QuotientRemainder.java
@@ -1,12 +1,12 @@
+
 import java.util.Scanner;
 
 public class QuotientRemainder {
 
-    // Method to find remainder and quotient of a number divided by divisor
     public static int[] findRemainderAndQuotient(int number, int divisor) {
         int quotient = number / divisor;
         int remainder = number % divisor;
-        return new int[] {remainder, quotient};
+        return new int[]{remainder, quotient};
     }
 
     public static void main(String[] args) {
