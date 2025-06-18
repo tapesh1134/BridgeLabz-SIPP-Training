95c0875adb7027e6dc211d5d220d08dea8d8f65b
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 11:26:55 +0530

    Refactor: improve code formatting in OTPGenerator class

--- Methods/OTPGenerator.java
+++ Methods/OTPGenerator.java
@@ -1,14 +1,13 @@
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class OTPGenerator {
 
-    // Method to generate a 6-digit OTP
     public static int generateOTP() {
-        return 100000 + (int)(Math.random() * 900000);
+        return 100000 + (int) (Math.random() * 900000);
     }
 
-    // Method to generate 10 OTPs and store in an array
     public static int[] generateOTPs(int count) {
         int[] otps = new int[count];
         for (int i = 0; i < count; i++) {
@@ -17,12 +16,11 @@ public class OTPGenerator {
         return otps;
     }
 
-    // Method to check if all OTPs are unique
     public static boolean areOTPsUnique(int[] otps) {
         Set<Integer> set = new HashSet<>();
         for (int otp : otps) {
             if (!set.add(otp)) {
-                return false; // duplicate found
+                return false;
             }
         }
         return true;
