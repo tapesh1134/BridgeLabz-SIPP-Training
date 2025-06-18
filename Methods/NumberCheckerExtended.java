2d8e14e94781e9e3e7d2a95975efeab7e725dcff
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 11:24:52 +0530

    Refactor: improve code clarity in NumberCheckerExtended class

--- Methods/NumberCheckerExtended.java
+++ Methods/NumberCheckerExtended.java
@@ -1,13 +1,12 @@
+
 import java.util.Arrays;
 
 public class NumberCheckerExtended {
 
-    // Method to find count of digits in a number
     public static int countDigits(int number) {
         return String.valueOf(Math.abs(number)).length();
     }
 
-    // Method to store digits of a number in an array
     public static int[] digitsArray(int number) {
         String numStr = String.valueOf(Math.abs(number));
         int[] digits = new int[numStr.length()];
@@ -17,7 +16,6 @@ public class NumberCheckerExtended {
         return digits;
     }
 
-    // Method to find sum of digits using digits array
     public static int sumOfDigits(int number) {
         int[] digits = digitsArray(number);
         int sum = 0;
@@ -27,7 +25,6 @@ public class NumberCheckerExtended {
         return sum;
     }
 
-    // Method to find sum of squares of digits using Math.pow()
     public static int sumOfSquaresOfDigits(int number) {
         int[] digits = digitsArray(number);
         int sum = 0;
@@ -37,14 +34,11 @@ public class NumberCheckerExtended {
         return sum;
     }
 
-    // Method to check if a number is a Harshad number
     public static boolean isHarshadNumber(int number) {
         int sum = sumOfDigits(number);
         return number % sum == 0;
     }
 
-    // Method to find frequency of each digit in the number
-    // Returns a 2D array: first column digit, second column frequency
     public static int[][] digitFrequency(int number) {
         int[] digits = digitsArray(number);
         int[] freq = new int[10];
@@ -53,7 +47,9 @@ public class NumberCheckerExtended {
         }
         int count = 0;
         for (int f : freq) {
-            if (f > 0) count++;
+            if (f > 0) {
+                count++;
+            }
         }
         int[][] result = new int[count][2];
         int index = 0;
