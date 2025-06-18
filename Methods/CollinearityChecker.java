0ce1b5f8ebbbb9ee03364904234c91456860a912
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 11:06:49 +0530

    Refactor: Remove redundant comments and improve code formatting in CollinearityChecker class

--- Methods/CollinearityChecker.java
+++ Methods/CollinearityChecker.java
@@ -1,16 +1,14 @@
+
 import java.util.Scanner;
 
 public class CollinearityChecker {
 
-    // Method to check collinearity using slope formula
     public static boolean areCollinearBySlope(int x1, int y1, int x2, int y2, int x3, int y3) {
-        // To avoid division by zero, use cross multiplication
         return (y2 - y1) * (x3 - x2) == (y3 - y2) * (x2 - x1);
     }
 
-    // Method to check collinearity using area of triangle formula
     public static boolean areCollinearByArea(int x1, int y1, int x2, int y2, int x3, int y3) {
-        int area = x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2);
+        int area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
         return area == 0;
     }
 
