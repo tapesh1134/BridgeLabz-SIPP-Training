348e5c05229051808520ef82a1818fa4e5e9f3b5
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 11:27:53 +0530

    Refactor: improve code formatting in SimpleInterestCalculator class

--- Methods/SimpleInterestCalculator.java
+++ Methods/SimpleInterestCalculator.java
@@ -1,8 +1,8 @@
+
 import java.util.Scanner;
 
 public class SimpleInterestCalculator {
 
-    // Method to calculate simple interest
     public static double calculateSimpleInterest(double principal, double rate, double time) {
         return (principal * rate * time) / 100.0;
     }
@@ -22,7 +22,7 @@ public class SimpleInterestCalculator {
         double simpleInterest = calculateSimpleInterest(principal, rate, time);
 
         System.out.printf("The Simple Interest is %.2f for Principal %.2f, Rate of Interest %.2f and Time %.2f%n",
-                          simpleInterest, principal, rate, time);
+                simpleInterest, principal, rate, time);
 
         scanner.close();
     }
