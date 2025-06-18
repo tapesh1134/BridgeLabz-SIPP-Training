142a6e3ac265ea620902d530126fbc2876801bac
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 11:23:30 +0530

    Refactor: improve code formatting and readability in MatrixOperations class

--- Methods/MatrixOperations.java
+++ Methods/MatrixOperations.java
@@ -1,21 +1,20 @@
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class MatrixOperations {
 
-    // Method to create a random matrix with given rows and columns
     public static int[][] createRandomMatrix(int rows, int cols) {
         Random rand = new Random();
         int[][] matrix = new int[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                matrix[i][j] = rand.nextInt(10); // random numbers 0-9
+                matrix[i][j] = rand.nextInt(10);
             }
         }
         return matrix;
     }
 
-    // Method to find the transpose of a matrix
     public static int[][] transpose(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
@@ -30,14 +29,14 @@ public class MatrixOperations {
 
     // Method to find determinant of 2x2 matrix
     public static int determinant2x2(int[][] matrix) {
-        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
     }
 
     // Method to find determinant of 3x3 matrix
     public static int determinant3x3(int[][] matrix) {
-        int det = matrix[0][0]*(matrix[1][1]*matrix[2][2] - matrix[1][2]*matrix[2][1])
-                - matrix[0][1]*(matrix[1][0]*matrix[2][2] - matrix[1][2]*matrix[2][0])
-                + matrix[0][2]*(matrix[1][0]*matrix[2][1] - matrix[1][1]*matrix[2][0]);
+        int det = matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1])
+                - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
+                + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
         return det;
     }
 
@@ -48,10 +47,10 @@ public class MatrixOperations {
             return null; // no inverse
         }
         double[][] inverse = new double[2][2];
-        inverse[0][0] = matrix[1][1] / (double)det;
-        inverse[0][1] = -matrix[0][1] / (double)det;
-        inverse[1][0] = -matrix[1][0] / (double)det;
-        inverse[1][1] = matrix[0][0] / (double)det;
+        inverse[0][0] = matrix[1][1] / (double) det;
+        inverse[0][1] = -matrix[0][1] / (double) det;
+        inverse[1][0] = -matrix[1][0] / (double) det;
+        inverse[1][1] = matrix[0][0] / (double) det;
         return inverse;
     }
 
@@ -63,17 +62,17 @@ public class MatrixOperations {
         }
         double[][] inverse = new double[3][3];
         // Calculate cofactors matrix
-        inverse[0][0] =  (matrix[1][1]*matrix[2][2] - matrix[1][2]*matrix[2][1]);
-        inverse[0][1] = -(matrix[0][1]*matrix[2][2] - matrix[0][2]*matrix[2][1]);
-        inverse[0][2] =  (matrix[0][1]*matrix[1][2] - matrix[0][2]*matrix[1][1]);
+        inverse[0][0] = (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]);
+        inverse[0][1] = -(matrix[0][1] * matrix[2][2] - matrix[0][2] * matrix[2][1]);
+        inverse[0][2] = (matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1]);
 
-        inverse[1][0] = -(matrix[1][0]*matrix[2][2] - matrix[1][2]*matrix[2][0]);
-        inverse[1][1] =  (matrix[0][0]*matrix[2][2] - matrix[0][2]*matrix[2][0]);
-        inverse[1][2] = -(matrix[0][0]*matrix[1][2] - matrix[0][2]*matrix[1][0]);
+        inverse[1][0] = -(matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]);
+        inverse[1][1] = (matrix[0][0] * matrix[2][2] - matrix[0][2] * matrix[2][0]);
+        inverse[1][2] = -(matrix[0][0] * matrix[1][2] - matrix[0][2] * matrix[1][0]);
 
-        inverse[2][0] =  (matrix[1][0]*matrix[2][1] - matrix[1][1]*matrix[2][0]);
-        inverse[2][1] = -(matrix[0][0]*matrix[2][1] - matrix[0][1]*matrix[2][0]);
-        inverse[2][2] =  (matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]);
+        inverse[2][0] = (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+        inverse[2][1] = -(matrix[0][0] * matrix[2][1] - matrix[0][1] * matrix[2][0]);
+        inverse[2][2] = (matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]);
 
         // Transpose cofactors matrix and divide by determinant
         double[][] adjoint = transposeDouble(inverse);
