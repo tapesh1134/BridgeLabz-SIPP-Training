2352f37409dcd06a5247374a8282d9bb449c0778
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 11:22:09 +0530

    Refactor: improve code in EmployeeBonusCalculator class

--- Methods/EmployeeBonusCalculator.java
+++ Methods/EmployeeBonusCalculator.java
@@ -1,21 +1,20 @@
+
 import java.util.Random;
 
 public class EmployeeBonusCalculator {
 
-    // Method to generate random salary and years of service for 10 employees
     public static int[][] generateEmployeeData() {
         Random rand = new Random();
-        int[][] data = new int[10][2]; // [][0] = salary, [][1] = years of service
+        int[][] data = new int[10][2];
         for (int i = 0; i < 10; i++) {
-            data[i][0] = 10000 + rand.nextInt(90000); // 5-digit salary between 10000 and 99999
-            data[i][1] = rand.nextInt(11); // years of service between 0 and 10
+            data[i][0] = 10000 + rand.nextInt(90000);
+            data[i][1] = rand.nextInt(11);
         }
         return data;
     }
 
-    // Method to calculate new salary and bonus based on years of service
     public static double[][] calculateNewSalaryAndBonus(int[][] data) {
-        double[][] result = new double[10][2]; // [][0] = new salary, [][1] = bonus amount
+        double[][] result = new double[10][2];
         for (int i = 0; i < 10; i++) {
             int salary = data[i][0];
             int years = data[i][1];
@@ -28,7 +27,6 @@ public class EmployeeBonusCalculator {
         return result;
     }
 
-    // Method to calculate and display sums and details in tabular format
     public static void displaySummary(int[][] data, double[][] newData) {
         double sumOldSalary = 0;
         double sumNewSalary = 0;
@@ -43,7 +41,7 @@ public class EmployeeBonusCalculator {
             sumOldSalary += oldSalary;
             sumNewSalary += newSalary;
             totalBonus += bonus;
-            System.out.printf("%d\t%d\t\t%d\t%.2f\t%.2f\n", i+1, oldSalary, years, bonus, newSalary);
+            System.out.printf("%d\t%d\t\t%d\t%.2f\t%.2f\n", i + 1, oldSalary, years, bonus, newSalary);
         }
         System.out.println("-----------------------------------------------------");
         System.out.printf("Total\t%.2f\t\t\t%.2f\t%.2f\n", sumOldSalary, totalBonus, sumNewSalary);
