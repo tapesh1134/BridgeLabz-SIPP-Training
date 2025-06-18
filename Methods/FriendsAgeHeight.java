2a3fcb6cf3c90456ded0d312df6843c4dbdced49
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-18 09:31:06 +0530

    Added: Implement Friends Age and Height program to find the youngest and tallest friends based on user input

---
+++ Methods/FriendsAgeHeight.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class FriendsAgeHeight {
+
+    // Method to find the index of the youngest friend
+    public static int findYoungest(int[] ages) {
+        int youngestIndex = 0;
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+        return youngestIndex;
+    }
+
+    // Method to find the index of the tallest friend
+    public static int findTallest(double[] heights) {
+        int tallestIndex = 0;
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+        return tallestIndex;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = scanner.nextInt();
+
+            System.out.print("Enter height of " + names[i] + " in meters: ");
+            heights[i] = scanner.nextDouble();
+        }
+
+        int youngestIndex = findYoungest(ages);
+        int tallestIndex = findTallest(heights);
+
+        System.out.println(names[youngestIndex] + " is the youngest friend.");
+        System.out.println(names[tallestIndex] + " is the tallest friend.");
+
+        scanner.close();
+    }
+}
