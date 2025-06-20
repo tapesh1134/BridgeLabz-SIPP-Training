c97521e3030013f2ef8884701bf3c7b5569bfb26
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-20 10:27:13 +0530

    Added: GCDLCMCalculator class to compute and display the GCD and LCM of two user-input numbers

---
+++ BuiltInFunction/GCDLCMCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GCDLCMCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        int gcd = calculateGCD(num1, num2);
+        int lcm = calculateLCM(num1, num2, gcd);
+        
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+        
+        scanner.close();
+    }
+    
+    static int calculateGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    static int calculateLCM(int a, int b, int gcd) {
+        return Math.abs(a * b) / gcd;
+    }
+}
