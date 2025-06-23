52850651193f35c5a3efc3680681da6080fc0af5
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-23 11:43:34 +0530

    Added: Implement SubstringOccurrences class to count occurrences of a substring in a string

---
+++ Extra Java String/SubstringOccurrences.java
@@ -0,0 +1,15 @@
+
+public class SubstringOccurrences {
+
+    public static void main(String[] args) {
+        String str = "banana bandana banana";
+        String sub = "ana";
+        int count = 0;
+        for (int i = 0; i <= str.length() - sub.length(); i++) {
+            if (str.substring(i, i + sub.length()).equals(sub)) {
+                count++;
+            }
+        }
+        System.out.println("Occurrences of '" + sub + "': " + count);
+    }
+}
