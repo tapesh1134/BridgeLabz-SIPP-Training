cae7be7d870ab763dc004ee3d0cf7ef64dfdc814
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-24 11:36:17 +0530

    Added: Student class with attributes for rollNumber, name, and CGPA; included a constructor, getter and setter for CGPA, and a PostgraduateStudent subclass with a method to display the name.

---
+++ ConstructorAndClassVariables/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+    public Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+    public double getCGPA() {
+        return CGPA;
+    }
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+}
+class PostgraduateStudent extends Student {
+    public PostgraduateStudent(int rollNumber, String name, double CGPA) {
+        super(rollNumber, name, CGPA);
+    }
+    void showName() {
+        System.out.println(name);
+    }
+} 
