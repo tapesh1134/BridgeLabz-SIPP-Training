d4edf1c1c5fb751516708faa27b42ed31c66da35
Tapesh Sharma <tapeshs1134@gmail.com>
2025-06-24 09:28:41 +0530

    Added: Implement Employee class to manage employee details and display information

---
+++ OOPS/Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+    String name;
+    int id;
+    double salary;
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+    public static void main(String[] args) {
+        Employee emp = new Employee("John Doe", 101, 50000);
+        emp.displayDetails();
+    }
+} 
