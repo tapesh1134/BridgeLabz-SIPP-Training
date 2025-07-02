fb5ffbe4f01f4b6d8ff5db24fc776f1df567e468
Tapesh Sharma <tapeshs1134@gmail.com>
2025-07-02 08:59:31 +0530

    Added: Book and Author classes; implemented displayInfo methods and a main method to demonstrate functionality.

---
+++ Inheritance/LibraryBook.java
@@ -0,0 +1,38 @@
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Bio: " + bio);
+    }
+}
+
+public class LibraryBook {
+    public static void main(String[] args) {
+        Author author = new Author("The Great Book", 2021, "John Doe", "An experienced fiction writer.");
+        author.displayInfo();
+    }
+}
