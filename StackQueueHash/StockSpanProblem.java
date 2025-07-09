c829ed96a62dee93b875684efaffd17b47b1ab1b
Tapesh Sharma <tapeshs1134@gmail.com>
2025-07-09 12:12:36 +0530

    Added: Implementation of Stock Span Problem to calculate the span of stock prices using a stack

---
+++ StackQueueHash/StockSpanProblem.java
@@ -0,0 +1,23 @@
+package StackQueueHash;
+import java.util.Stack;
+class StockSpanProblem {
+    public int[] calculateSpan(int[] prices) {
+        int n = prices.length;
+        int[] span = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] <= prices[i]) {
+                stack.pop();
+            }
+            span[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+        return span;
+    }
+    public static void main(String[] args) {
+        StockSpanProblem s = new StockSpanProblem();
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+        int[] span = s.calculateSpan(prices);
+        for (int x : span) System.out.print(x + " ");
+    }
+} 
