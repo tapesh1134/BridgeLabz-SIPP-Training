e890b05146140e5d636e1e7cdc98ecae8c892cdd
Tapesh Sharma <tapeshs1134@gmail.com>
2025-07-09 12:25:31 +0530

    Added: Implementation of Heap Sort algorithm to sort job applicants' salaries

---
+++ Sorting/HeapSortJobApplicants.java
@@ -0,0 +1,30 @@
+package Sorting;
+class HeapSortJobApplicants {
+    public void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+    private void heapify(int[] arr, int n, int i) {
+        int largest = i, l = 2 * i + 1, r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest]) largest = l;
+        if (r < n && arr[r] > arr[largest]) largest = r;
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+    public static void main(String[] args) {
+        int[] salary = {35000, 50000, 42000, 60000, 39000};
+        HeapSortJobApplicants h = new HeapSortJobApplicants();
+        h.heapSort(salary);
+        for (int x : salary) System.out.print(x + " ");
+    }
+} 
