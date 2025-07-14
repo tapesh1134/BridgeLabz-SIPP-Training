871cdb63ebd3a9609d0dbf0332ec1dfda19d6ffe
Tapesh Sharma <tapeshs1134@gmail.com>
2025-07-14 09:57:09 +0530

    Added: Implementation of Binary Search to find a peak element in an array

---
+++ LinearBinarySeach/BinarySearchPeakElement.java
@@ -0,0 +1,22 @@
+public class BinarySearchPeakElement {
+    public static int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                right = mid - 1;
+            } else if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        int peakIndex = findPeakElement(nums);
+        System.out.println("Peak element index: " + peakIndex);
+    }
+}
