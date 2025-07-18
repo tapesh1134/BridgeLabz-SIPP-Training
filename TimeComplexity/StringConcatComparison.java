18d0450752dfa70da31307a337202ae5537c1a36
tapesh1134 <tapeshs1134@gmail.com>
2025-07-18 08:45:46 +0530

    Added: Implementation of String Concat Comparison measuring performance of String, StringBuilder, and StringBuffer for varying sizes

---
+++ TimeComplexity/StringConcatComparison.java
@@ -0,0 +1,20 @@
+public class StringConcatComparison {
+    public static void main(String[] args) {
+        int[] sizes = {1000, 10000, 1000000};
+        for (int n : sizes) {
+            long start = System.nanoTime();
+            String s = "";
+            for (int i = 0; i < n; i++) s += "a";
+            long stringTime = System.nanoTime() - start;
+            start = System.nanoTime();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) sb.append("a");
+            long sbTime = System.nanoTime() - start;
+            start = System.nanoTime();
+            StringBuffer sbf = new StringBuffer();
+            for (int i = 0; i < n; i++) sbf.append("a");
+            long sbfTime = System.nanoTime() - start;
+            System.out.println(n+" " + stringTime/1e6 + "ms " + sbTime/1e6 + "ms " + sbfTime/1e6 + "ms");
+        }
+    }
+} 
