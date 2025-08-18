cd1b065ac2eb21a23f65fc6e3037e6d59cb512eb
tapesh1134 <tapeshs1134@gmail.com>
2025-08-18 21:13:07 +0530

    Added: CapitalizedWords class to demonstrate regex matching for capitalized words in a given text.

---
+++ Regex_&_unit/CapitalizedWords.java
@@ -0,0 +1,12 @@
+
+import java.util.regex.*;
+
+public class CapitalizedWords {
+    public static void main(String[] args) {
+        String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+        Matcher m = Pattern.compile("\\b[A-Z][a-z]*\\b").matcher(text);
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
