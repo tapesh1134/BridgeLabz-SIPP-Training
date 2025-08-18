03506233911234267adf3ae6500868871d93149c
tapesh1134 <tapeshs1134@gmail.com>
2025-08-18 21:14:17 +0530

    Added: MultipleSpaces class to replace multiple spaces in a string with a single space.

---
+++ Regex_&_unit/MultipleSpaces.java
@@ -0,0 +1,9 @@
+
+public class MultipleSpaces {
+    public static void main(String[] args) {
+        String text = "This   is   an   example    with   multiple   spaces.";
+        String result = text.replaceAll("\\s+", " ");
+        System.out.println(result);
+    }
+}
+ 
