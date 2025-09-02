3cfe8fd396860f0f96fe13018f8d05663c022396
tapesh1134 <tapeshs1134@gmail.com>
2025-09-02 09:45:34 +0530

    Added: PatientIDPrinting class to demonstrate the use of lambda expressions for printing a list of patient IDs in the main method.

---
+++ LambdaExpression/PatientIDPrinting.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class PatientIDPrinting {
+    public static void main(String[] args) {
+        List<String> patientIDs = Arrays.asList("P001", "P002", "P003");
+
+        
+        patientIDs.forEach(System.out::println);
+    }
+}
+ 
