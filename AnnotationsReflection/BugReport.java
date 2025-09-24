c1bb820e1f7dfda5cce9aea00769594a4214eda3
tapesh1134 <tapeshs1134@gmail.com>
2025-09-24 07:11:04 +0530

    Added: BugReport and BugReports annotations to facilitate the use of repeatable bug reporting in Java methods.

---
+++ AnnotationsReflection/BugReport.java
@@ -0,0 +1,12 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Repeatable(BugReports.class)
+public @interface BugReport {
+    String description();
+}
